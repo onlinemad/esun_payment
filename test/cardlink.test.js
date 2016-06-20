dc23543f2b4208162d1bbf37f5270e3d2cf56570
Ian Wu <onlinemad@gmail.com>
2016-06-20 23:24:59 +0800

    modified cradling.mac set input_encoding as utf8

--- test/cardlink.test.js
+++ test/cardlink.test.js
@@ -7,7 +7,7 @@ describe('cardlink', function() {
     KEY: '123456789ABCDEF123456789ABCDEF'
   }
   describe('#mac()', function() {
-    var mac = cardlink.mac({TxnType:'A',Account:'testacct'}, sample_config.KEY);
+    var mac = cardlink.mac(JSON.stringify({TxnType:'A',Account:'testacct'}), sample_config.KEY);
     expect(mac).to.equal('6125537529184f83096449d5e04a68732a72ce434e580940dccd73d2ced3d389');
   });
   describe('#hash()', function() {
