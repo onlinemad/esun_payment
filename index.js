0c87c052c35b6bff2fc9e2d45dadfba073224507
Ian Wu <onlinemad@gmail.com>
2016-06-20 21:13:49 +0800

    Added cardlink support

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports.cardlink = require('./lib/cardlink');
---
+++ test/cardlink.test.js
@@ -0,0 +1,14 @@
+var expect = require('chai').expect;
+var cardlink = require('../lib/cardlink');
+
+describe('cardlink', function() {
+  var sample_config = {
+    SID: '',
+    KEY: '123456789ABCDEF123456789ABCDEF'
+  }
+  describe('#hash()', function() {
+    var esun = new cardlink(sample_config);
+    var hashed_payload = esun.hash({TxnType:'A',Account:'testacct'});
+    expect(hashed_payload.mac).to.equal('6125537529184f83096449d5e04a68732a72ce434e580940dccd73d2ced3d389');
+  });
+})
