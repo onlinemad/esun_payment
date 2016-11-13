7585c5da05d53bc45c5a10659681ae1d8b3a868a
Ian Wu <onlinemad@gmail.com>
2016-11-13 21:04:14 +0800

    增加 statement() 來產生請款檔

--- test/esun.test.js
+++ test/esun.test.js
@@ -8,6 +8,7 @@ describe('esun', () => {
     SID: '',
     KEY: '123456789ABCDEF123456789ABCDEF'
   }
+  var esun = new ESUN(sample_config)
   it('#mac()', () => {
     var mac = ESUN.mac(JSON.stringify({
       TxnType: 'A',
@@ -16,11 +17,36 @@ describe('esun', () => {
     expect(mac).to.equal('6125537529184f83096449d5e04a68732a72ce434e580940dccd73d2ced3d389')
   })
   it('#hash()', () => {
-    var esun = new ESUN(sample_config)
     var hashed_payload = esun.hash({
       TxnType: 'A',
       Account: 'testacct'
     })
     expect(hashed_payload.mac).to.equal('6125537529184f83096449d5e04a68732a72ce434e580940dccd73d2ced3d389')
+  })
+  it('#statement()', () => {
+    var stores = [{
+      sid: '1234567890',
+      orders: [{
+        id: '1234567890',
+        type: '05',
+        amount: 100
+      }, {
+        id: '0987654321',
+        type: '06',
+        amount: 200
+      }]
+    }, {
+      sid: '0987654321',
+      orders: [{
+        id: '1234567890',
+        type: '05',
+        amount: 100
+      }, {
+        id: '0987654321',
+        type: '06',
+        amount: 200
+      }]
+    }]
+    var statement = ESUN.statement('6789012345', stores)
   })
 })
